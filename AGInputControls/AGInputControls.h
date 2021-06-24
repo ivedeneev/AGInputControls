2bf5eddc26d6efa22fa835b2693fde351aaf79ec
Igor Vedeneev <i.vedeneev@agima.ru>
2021-06-24 16:14:01 +0300

    Initial Commit

---
+++ AGInputControls/AGInputControls.h
@@ -0,0 +1,18 @@
+//
+//  AGInputControls.h
+//  AGInputControls
+//
+//  Created by Igor Vedeneev on 6/24/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AGInputControls.
+FOUNDATION_EXPORT double AGInputControlsVersionNumber;
+
+//! Project version string for AGInputControls.
+FOUNDATION_EXPORT const unsigned char AGInputControlsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AGInputControls/PublicHeader.h>
+
+
