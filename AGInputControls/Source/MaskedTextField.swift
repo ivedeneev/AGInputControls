5fe225f21dd34c03f27ddced8db7f72823bb41bd
Igor Vedeneev <i.vedeneev@agima.ru>
2021-10-15 15:11:12 +0300

    improve formatting logic

--- AGInputControls/Source/MaskedTextField.swift
+++ AGInputControls/Source/MaskedTextField.swift
@@ -16,36 +16,56 @@ open class MaskedTextField: FormattingTextField {
         guard let mask = formattingMask, let t = text, !t.isEmpty else { return text }
 
         var result = ""
-        if exampleMask != nil {
-            setNeedsDisplay()
-        }
         
-        for  (idx, maskSymbol) in mask.enumerated() {
+        var isSymbolInserted = false
+        var symbol: Character?
+        
+        for (idx, maskSymbol) in mask.enumerated() {
+            
             let index = t.index(t.startIndex, offsetBy: idx)
+            
             if idx == t.count {
-                if !(maskSymbol.isLetter || maskSymbol.isNumber), !result.isEmpty {
-                    result.append(maskSymbol)
+                if !(maskSymbol.isLetter || maskSymbol.isNumber), !result.isEmpty, isSymbolInserted {
+                    symbol = maskSymbol
                 }
                 break
             }
+            
             let ch = t[index]
+            
             switch maskSymbol {
             case "L":
+                isSymbolInserted = ch.isLetter
                 if ch.isLetter {
                     result.append(ch)
                 }
             case "D":
+                isSymbolInserted = ch.isNumber
                 if ch.isNumber {
                     result.append(ch)
                 }
             case "A":
+                isSymbolInserted = ch.isNumber || ch.isLetter
                 if ch.isNumber || ch.isLetter  {
                     result.append(ch)
                 }
             default:
                 result.append(maskSymbol)
+                isSymbolInserted = true
             }
         }
+        
+        if let s = symbol,
+           isSymbolInserted,
+           mask[mask.index(mask.startIndex, offsetBy: result.count)] == s
+        {
+            result.append(s)
+        }
+        
+        if exampleMask != nil {
+            setNeedsDisplay()
+        }
+        
         return result.uppercased()
     }
 
