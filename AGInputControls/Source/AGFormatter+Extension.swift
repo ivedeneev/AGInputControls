9eb14c3225145358846162990e7152aa3c0baae8
Igor Vedeneev <i.vedeneev@agima.ru>
2021-10-30 16:27:46 +0300

    refacvtor

---
+++ AGInputControls/Source/AGFormatter+Extension.swift
@@ -0,0 +1,82 @@
+//
+//  AGFormatter+Extension.swift
+//  AGInputControls
+//
+//  Created by Igor Vedeneev on 30.10.2021.
+//
+
+import Foundation
+
+extension AGFormatter {
+    public var maskHasConstantPrefix: Bool {
+        prefix.first(where: { "#?*".contains($0) }) == nil && !prefix.isEmpty
+    }
+    
+    public func isValidCharachter(_ ch: Character?) -> Bool {
+        guard let char = ch else { return false }
+        return char.isNumber || char.isLetter
+    }
+    
+    
+    public func isValidString(text: String?) -> Bool {
+        guard let resultText = formattedText(text: text) else { return allowsEmptyOrNilStrings }
+        return resultText.count == mask.count
+    }
+    
+    public var allowsEmptyOrNilStrings: Bool {
+        false
+    }
+    
+    public func formattedText(text: String?) -> String? {
+        guard let t = text else { return text }
+
+        var textRemovingSpecialSymbols = String(t.filter { isValidCharachter($0) })
+
+        // insert prefix if its constant
+        if maskHasConstantPrefix,
+           !textRemovingSpecialSymbols.hasPrefix(prefix) {
+            textRemovingSpecialSymbols.insert(
+                contentsOf: prefix,
+                at: textRemovingSpecialSymbols.startIndex
+            )
+        }
+        
+        var result = ""
+        var index = maskHasConstantPrefix ?
+        textRemovingSpecialSymbols.index(textRemovingSpecialSymbols.startIndex, offsetBy: prefix.count) :
+        textRemovingSpecialSymbols.startIndex
+        for ch in mask where index < textRemovingSpecialSymbols.endIndex {
+            switch ch {
+            case "*", "#", "?":
+                while // trim 'bad' charachters
+                    index < textRemovingSpecialSymbols.endIndex &&
+                        (textRemovingSpecialSymbols[index].isNumber && ch == "*" ||
+                         textRemovingSpecialSymbols[index].isLetter && ch == "#")
+                {
+                    textRemovingSpecialSymbols.remove(at: index)
+                }
+                
+                //double check if we are not out of bounds, because we manupulate with string length inside
+                if index >= textRemovingSpecialSymbols.endIndex {
+                    break
+                }
+                
+                result.append(textRemovingSpecialSymbols[index])
+                index = textRemovingSpecialSymbols.index(after: index)
+            default:
+                result.append(ch)
+            }
+        }
+        
+        if textRemovingSpecialSymbols == prefix && maskHasConstantPrefix {
+            return textRemovingSpecialSymbols.uppercased()
+        }
+        
+        // consider string which contains only special symbols and spases invalid
+        if result.filter({ $0.isLetter || $0.isNumber }).isEmpty {
+            return nil
+        }
+        
+        return result.uppercased().trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+}
--- AGInputControls/Source/DefaultFormatter.swift
+++ AGInputControls/Source/DefaultFormatter.swift
@@ -8,72 +8,18 @@
 import Foundation
 
 public struct DefaultFormatter: AGFormatter {
-    
-    public var mask: String
-    public var allowsEmptyOrNilStrings: Bool
-    
-    public var prefix: String {
-        guard let separator = mask.first(where: { !("#?*".contains($0) || $0.isLetter || $0.isNumber) }) else { return "" }
-        
-        return mask.components(separatedBy: String(separator)).first ?? ""
-    }
-    
+
+    public let mask: String
+    public let allowsEmptyOrNilStrings: Bool
     
     public init(mask: String, allowsEmptyOrNilStrings: Bool = false) {
         self.mask = mask
         self.allowsEmptyOrNilStrings = allowsEmptyOrNilStrings
     }
     
-    public func formattedText(text: String?) -> String? {
-        guard let t = text else { return text }
-
-        var textRemovingSpecialSymbols = String(t.filter { isValidCharachter($0) })
-
-        // insert prefix if its constant
-        if maskHasConstantPrefix,
-           !textRemovingSpecialSymbols.hasPrefix(prefix) {
-            textRemovingSpecialSymbols.insert(
-                contentsOf: prefix,
-                at: textRemovingSpecialSymbols.startIndex
-            )
-        }
+    public var prefix: String {
+        guard let separator = mask.first(where: { !("#?*".contains($0) || $0.isLetter || $0.isNumber) }) else { return "" }
         
-        var result = ""
-        var index = maskHasConstantPrefix ?
-        textRemovingSpecialSymbols.index(textRemovingSpecialSymbols.startIndex, offsetBy: prefix.count) :
-        textRemovingSpecialSymbols.startIndex
-        for ch in mask where index < textRemovingSpecialSymbols.endIndex {
-            switch ch {
-            case "*", "#", "?":
-                while // trim 'bad' charachters
-                    index < textRemovingSpecialSymbols.endIndex &&
-                        (textRemovingSpecialSymbols[index].isNumber && ch == "*" ||
-                         textRemovingSpecialSymbols[index].isLetter && ch == "#")
-                {
-                    textRemovingSpecialSymbols.remove(at: index)
-                }
-                
-                //double check if we are not out of bounds, because we manupulate with string length inside
-                if index >= textRemovingSpecialSymbols.endIndex {
-                    break
-                }
-                
-                result.append(textRemovingSpecialSymbols[index])
-                index = textRemovingSpecialSymbols.index(after: index)
-            default:
-                result.append(ch)
-            }
-        }
-        
-        if textRemovingSpecialSymbols == prefix && maskHasConstantPrefix {
-            return textRemovingSpecialSymbols.uppercased()
-        }
-        
-        // consider string which contains only special symbols and spases invalid
-        if result.filter({ $0.isLetter || $0.isNumber }).isEmpty {
-            return nil
-        }
-        
-        return result.uppercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        return mask.components(separatedBy: String(separator)).first ?? ""
     }
 }
