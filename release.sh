24b8e2f8ad48de950bcddf8803ded88cf0496198
Igor Vedeneev <getmaxx@hotmail.com>
2024-03-23 18:21:33 +0100

    update version

--- release.sh
+++ release.sh
@@ -4,14 +4,17 @@ set -e
 # Constants
 PODSPEC_PATH=AGInputControls.podspec
 
-
-# bump version if needed (if major & patch versions )
-agvtool next-version -all 
-
 # update podspec && push
 CURRENT_VERSION=`agvtool mvers -terse1`
-CURRENT_BUILD=`agvtool vers -terse`
+NEW_VERSION='1.1.4'
 
-echo ""
-echo "Current Version: ${CURRENT_VERSION} (${CURRENT_BUILD})"
-echo ""
+sed -i '' -e "s/$CURRENT_VERSION/$NEW_VERSION/" $PODSPEC_PATH
+# bump version if needed (if major & patch versions )
+# agvtool new-marketing-version 1.1.4
+LAST_TAG=`git describe --abbrev=0`
+# echo ""
+# echo "Current Version: ${CURRENT_VERSION} (${CURRENT_BUILD})"
+# echo ""
+
+# git tag $CURRENT_VERSION
+# git push origin $CURRENT_VERSION
